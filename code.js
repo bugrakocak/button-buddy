2aa630bee9efedd4b1142fcf9d7d4da320a6d2fd
Buğra Koçak <mbkocak2@gmail.com>
2020-09-28 22:24:53 +0300

    Create states

--- code.js
+++ code.js
@@ -8,66 +8,84 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 function hexToRgb(hex) { var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex); return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : null; }
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-    });
-}
-main().then(() => {
+(() => __awaiter(this, void 0, void 0, function* () {
+    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
     figma.showUI(__html__);
-    const createIcon = (color) => {
-        const icon = figma.createNodeFromSvg(`<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10 19.6A9.6 9.6 0 1010 .4a9.6 9.6 0 000 19.2zm4.448-11.152a1.2 1.2 0 00-1.696-1.696L8.8 10.703 7.248 9.152a1.2 1.2 0 00-1.696 1.696l2.4 2.4a1.2 1.2 0 001.696 0l4.8-4.8z" fill="${color}"/></svg>`);
-        icon.layoutMode = "HORIZONTAL";
-        icon.name = "bugra";
-        icon.counterAxisSizingMode = "AUTO";
-        icon.verticalPadding = 2;
-        icon.horizontalPadding = 2;
-        return icon;
+    const sizes = ['small', 'medium', 'large'];
+    const states = ['empty', 'hover', 'disabled', 'focus'];
+    const layouts = ['noIcon', 'rightIcon', 'leftIcon'];
+    const createSideIcon = (color) => {
+        const iconNode = figma.createNodeFromSvg(`<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10 19.6A9.6 9.6 0 1010 .4a9.6 9.6 0 000 19.2zm4.448-11.152a1.2 1.2 0 00-1.696-1.696L8.8 10.703 7.248 9.152a1.2 1.2 0 00-1.696 1.696l2.4 2.4a1.2 1.2 0 001.696 0l4.8-4.8z" fill="${color}"/></svg>`);
+        iconNode.layoutMode = "HORIZONTAL";
+        iconNode.name = "sideIcon";
+        iconNode.counterAxisSizingMode = "AUTO";
+        iconNode.verticalPadding = 2;
+        iconNode.horizontalPadding = 2;
+        return iconNode;
+    };
+    const createText = (hexColor) => {
+        const text = figma.createText();
+        text.fontSize = 16;
+        text.lineHeight = {
+            unit: "PIXELS",
+            value: 24
+        };
+        text.characters = "BUTTON";
+        const rgb = hexToRgb(hexColor);
+        text.fills = [{ type: 'SOLID', color: { r: rgb.r / 255, g: rgb.g / 255, b: rgb.b / 255 } }];
+        return text;
+    };
+    const createButton = ({ borderRadius, style, color, }) => {
+        const buttonComponent = figma.createComponent();
+        const text = createText(color);
+        const vectorRight = createSideIcon(color);
+        const vectorLeft = createSideIcon(color);
+        vectorLeft.name = 'vectorLeft';
+        vectorRight.name = 'vectorRight';
+        buttonComponent.name = `${style} / Master`;
+        // buttonComponent.x = i * 150;
+        buttonComponent.layoutMode = "HORIZONTAL";
+        buttonComponent.counterAxisSizingMode = "AUTO";
+        buttonComponent.horizontalPadding = 24;
+        buttonComponent.verticalPadding = 12;
+        buttonComponent.itemSpacing = 4;
+        buttonComponent.cornerRadius = borderRadius;
+        buttonComponent.insertChild(0, vectorLeft);
+        buttonComponent.insertChild(1, text);
+        buttonComponent.insertChild(2, vectorRight);
+        if (style === "basic-solid") {
+            buttonComponent.fills = [{ type: 'SOLID', color: { r: 0 / 255, g: 56 / 255, b: 255 / 255 } }];
+        }
+        return buttonComponent;
     };
     figma.ui.onmessage = msg => {
+        const nodes = [];
         if (msg.type === 'button-maker') {
-            const nodes = [];
-            for (let i = 0; i < msg.count; i++) {
-                const rect = figma.createComponent();
-                rect.name = `${msg.styleValue} / Master`;
-                rect.createInstance;
-                const text = figma.createText();
-                const vectorRight = createIcon(msg.textColor);
-                const vectorLeft = createIcon(msg.textColor);
-                text.fontSize = 16;
-                text.lineHeight = {
-                    unit: "PIXELS",
-                    value: 24
-                };
-                text.characters = "BUTTON";
-                const rgb = hexToRgb(msg.textColor);
-                text.fills = [{ type: 'SOLID', color: { r: rgb.r / 255, g: rgb.g / 255, b: rgb.b / 255 } }];
-                console.log(rgb);
-                console.log([{ type: 'SOLID', color: { r: Number(`${rgb.r}`) / 255, g: Number(`${rgb.g}`) / 255, b: Number(`${rgb.b}`) / 255 } }]);
-                rect.x = i * 150;
-                rect.layoutMode = "HORIZONTAL";
-                rect.counterAxisSizingMode = "AUTO";
-                rect.horizontalPadding = 24;
-                rect.verticalPadding = 12;
-                rect.itemSpacing = 4;
-                rect.cornerRadius = msg.borderRadius;
-                rect.insertChild(0, vectorLeft);
-                rect.insertChild(1, text);
-                rect.insertChild(2, vectorRight);
-                if (msg.styleValue === "basic-solid") {
-                    rect.fills = [{ type: 'SOLID', color: { r: 0 / 255, g: 56 / 255, b: 255 / 255 } }];
+            const buttonCreator = () => createButton({ borderRadius: msg.borderRadius, style: msg.styleValue, color: msg.textColor });
+            sizes.forEach(size => {
+                if (size === 'small') {
+                    const smallMasterClone = buttonCreator();
+                    smallMasterClone.y = -200;
+                    figma.currentPage.appendChild(smallMasterClone);
+                    nodes.push(smallMasterClone);
+                    layouts.forEach(layout => {
+                        if (layout === 'noIcon') {
+                            states.forEach((state, i) => {
+                                const clone = buttonCreator();
+                                const children = clone.findChildren(node => node.type === 'VECTOR');
+                                clone.x = i * 200;
+                                clone.name = `${msg.styleValue}/${size}/${layout}/${state}`;
+                                children.forEach(c => c.visible = false);
+                                figma.currentPage.appendChild(clone);
+                                nodes.push(clone);
+                            });
+                        }
+                    });
                 }
-                if (msg.styleValue === "modern") {
-                    rect.fills = [{ type: 'GRADIENT_LINEAR', color: { r: 0 / 255, g: 56 / 255, b: 255 / 255 } }];
-                }
-                figma.currentPage.appendChild(rect);
-                nodes.push(rect);
-            }
+            });
             figma.currentPage.selection = nodes;
             figma.viewport.scrollAndZoomIntoView(nodes);
         }
-        // Make sure to close the plugin when you're done. Otherwise the plugin will
-        // keep running, which shows the cancel button at the bottom of the screen.
         figma.closePlugin();
     };
-});
+}))();
