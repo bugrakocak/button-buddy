2de315790c92cd8efd6cf292dafd10a4fa873b37
Buğra Koçak <mbkocak2@gmail.com>
2020-11-25 20:53:52 +0300

    Refactor

--- code.ts
+++ code.ts
@@ -41,16 +41,16 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
 
   const sizes = ['small', 'medium', 'large'];
   const states = ['idle', 'hover', 'focus', 'active', 'disabled'];
-  const layouts = ['Without Icon', 'With Icon'];
+  const layouts = ['withoutIcon', 'withIcon'];
 
   const iconsBySize = {
-    small: (textColor) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 16 16'><defs/><path fill='${textColor}' fill-rule='evenodd' d='M8 15A7 7 0 108 1a7 7 0 000 14zm3.244-8.131a.875.875 0 00-1.238-1.238L7.125 8.513 5.994 7.38A.875.875 0 004.756 8.62l1.75 1.75a.875.875 0 001.238 0l3.5-3.5z' clip-rule='evenodd'/></svg>`,
-    medium: (textColor) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'><defs/><path fill='${textColor}' fill-rule='evenodd' d='M12 22a10 10 0 100-20 10 10 0 000 20zm4.634-11.616a1.25 1.25 0 00-1.768-1.768l-4.116 4.117-1.616-1.617a1.25 1.25 0 00-1.768 1.768l2.5 2.5a1.25 1.25 0 001.768 0l5-5z' clip-rule='evenodd'/></svg>`,
-    large: (textColor) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'><defs/><path fill='${textColor}' fill-rule='evenodd' d='M16 30a14 14 0 100-28 14 14 0 000 28zm6.487-16.263a1.75 1.75 0 00-2.474-2.474l-5.763 5.763-2.263-2.263a1.75 1.75 0 00-2.474 2.474l3.5 3.5a1.75 1.75 0 002.474 0l7-7z' clip-rule='evenodd'/></svg>`,
+    small: (color) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 16 16'><defs/><path fill='${color}' fill-rule='evenodd' d='M8 15A7 7 0 108 1a7 7 0 000 14zm3.244-8.131a.875.875 0 00-1.238-1.238L7.125 8.513 5.994 7.38A.875.875 0 004.756 8.62l1.75 1.75a.875.875 0 001.238 0l3.5-3.5z' clip-rule='evenodd'/></svg>`,
+    medium: (color) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'><defs/><path fill='${color}' fill-rule='evenodd' d='M12 22a10 10 0 100-20 10 10 0 000 20zm4.634-11.616a1.25 1.25 0 00-1.768-1.768l-4.116 4.117-1.616-1.617a1.25 1.25 0 00-1.768 1.768l2.5 2.5a1.25 1.25 0 001.768 0l5-5z' clip-rule='evenodd'/></svg>`,
+    large: (color) => `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'><defs/><path fill='${color}' fill-rule='evenodd' d='M16 30a14 14 0 100-28 14 14 0 000 28zm6.487-16.263a1.75 1.75 0 00-2.474-2.474l-5.763 5.763-2.263-2.263a1.75 1.75 0 00-2.474 2.474l3.5 3.5a1.75 1.75 0 002.474 0l7-7z' clip-rule='evenodd'/></svg>`,
   }
 
-  const createSideIcon = (textColor, size, name) => {
-    const iconNode = figma.createNodeFromSvg(iconsBySize[size](textColor));
+  const createSideIcon = (color, size, name) => {
+    const iconNode = figma.createNodeFromSvg(iconsBySize[size](color));
     iconNode.name = name;
     return iconNode;
   };
@@ -71,7 +71,7 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
     text.fills = [{type: 'SOLID', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255}}];
     return text;
   }
-  
+
 
   const createButton = ({ borderRadius, style, textColor, size, strokeWeight, buttonColor }) => {
     const buttonComponent = figma.createComponent();
@@ -103,7 +103,6 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
     frame.insertChild(1, vector);
 
     buttonComponent.insertChild(0, frame);
-    
 
     if (style === 'BasicSolid') {
       buttonComponent.fills = [{type: 'SOLID', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255}}];
@@ -127,11 +126,11 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
 
 //TODO// Gradient is vertical but it's position is off on x axis.
     if (style === 'SoftGradient') {
-      
+
       buttonComponent.fills = [{type: 'GRADIENT_LINEAR',
-      gradientTransform: [ 
-        [0, 1, -1], 
-        [1, 0, 0] 
+      gradientTransform: [
+        [0, 1, -1],
+        [1, 0, 0]
       ],
           gradientStops: [
             {
@@ -145,6 +144,7 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
           ],
       }];
     }
+
     if (style === 'Glossy') {
       buttonComponent.fills = [{type: 'SOLID', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255}}];
       buttonComponent.effects = [
@@ -152,12 +152,14 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
         {type: 'INNER_SHADOW', color: {r: 255/255, g: 255/255, b: 255/255, a: 0.5}, offset: {x: 0, y: 4}, radius: 4, visible: true, blendMode: 'NORMAL' }
       ];
     }
+
     if (style === 'ComicLight') {
       buttonComponent.fills = [{type: 'SOLID', color: {r: 255/255, g: 255/255, b: 255/255}}];
       buttonComponent.strokeWeight = strokeWeight;
       buttonComponent.strokes = [{type: 'SOLID', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255}}];
       buttonComponent.effects = [{type: 'DROP_SHADOW', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255, a: 1}, offset: {x: -4, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }]
     }
+
     if (style === 'ComicBold') {
       buttonComponent.fills = [{type: 'SOLID', color: {r: rgb.r/255, g: rgb.g/255, b: rgb.b/255}}];
       buttonComponent.strokeWeight = strokeWeight;
@@ -175,20 +177,22 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
     //     {type: 'DROP_SHADOW', color: {r: 255/255, g: 255/255, b: 255/255, a: 0.3}, offset: {x: 0, y: 1}, radius: 1, visible: true, blendMode: 'NORMAL' }
     //   ];
     // }
+
     return buttonComponent;
   }
 
 
   const hideIconByLayout = (layout, component: ComponentNode) => {
-    if (layout === 'Without Icon') {
-      const icon = component.findAll(n => n.name === 'Icon');
-      icon.forEach(i => i.visible = false);
+    const icon = component.findOne(n => n.name === 'Icon');
+
+    if (layout === 'withoutIcon') {
+      icon.visible = false;
     }
 
-    if (layout === 'With Icon') {
-      const icon = component.findOne(n => n.name === 'Icon');
+    if (layout === 'withIcon') {
       icon.visible = true;
     }
+
     return component;
   }
 
@@ -205,85 +209,54 @@ function hexToRgb(hex) {  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$
     return hideIconByLayout(layout, instanceComponent);
   }
 
-  figma.ui.onmessage = msg => {
-    const nodes: SceneNode[] = [];
-    if (msg.type === 'button-maker') {
-      const buttonCreator = ({ size }: { size?: string; }) => createButton({ borderRadius: msg.borderRadius, style: msg.styleValue, textColor: msg.textColor, strokeWeight: msg.strokeWeight, buttonColor: msg.buttonColor, size });
-      sizes.forEach(size => {
-        if (size === 'small') {
-          const smallMain = buttonCreator({ size });
-          smallMain.name = `${msg.styleValue} Button Main`;
-          nodes.push(smallMain);
-          layouts.forEach(layout => {
-            if (layout === 'Without Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / false / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: smallMain, x: i * 300, y: 200, name, layout })
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
-            if (layout === 'With Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / true / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: smallMain, x: i * 300, y: 400, name, layout });
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
-          })
-        }
-        if (size === 'medium') {
-          const mediumMain = buttonCreator({ size });
-          mediumMain.name = `${msg.styleValue} Button Main`;
-          mediumMain.y = 1100;
-          figma.currentPage.appendChild(mediumMain);
-          nodes.push(mediumMain);
-          layouts.forEach(layout => {
-            if (layout === 'Without Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / false / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: mediumMain, x: i * 300, y: 1100 + 200, name, layout });
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
-            if (layout === 'With Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / true / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: mediumMain, x: i * 300, y: 1100 + 400, name, layout });
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
-          })
-        }
-        if (size === 'large') {
-          const largeMain = buttonCreator({ size });
-          largeMain.name = `${msg.styleValue} Button Main`;
-          largeMain.y = 2200;
-          figma.currentPage.appendChild(largeMain);
-          nodes.push(largeMain);
-          layouts.forEach(layout => {
-            if (layout === 'Without Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / false / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: largeMain, x: i * 300, y: 2200 + 200, name, layout });
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
+  const buttonCreator = ({ size, msg }: { size?: string; msg: any }) => createButton({
+    borderRadius: msg.borderRadius,
+    style: msg.styleValue,
+    textColor: msg.textColor,
+    strokeWeight: msg.strokeWeight,
+    buttonColor: msg.buttonColor,
+    size
+  });
 
-            if (layout === 'With Icon') {
-              states.forEach((state, i) => {
-                const name = `${msg.styleValue} Button / ${size} / true / ${state}`;
-                const instanceComponent = createInstanceComponent({ main: largeMain, x: i * 300, y: 2200 + 400, name, layout });
-                figma.currentPage.appendChild(instanceComponent);
-                nodes.push(instanceComponent);
-              })
-            }
-          })
-        }
+  const adjustButtonBySize = (size, msg, sizeIndex) => {
+    const nodes: SceneNode[] = [];
+    const button = buttonCreator({ size, msg });
+    button.name = `${msg.styleValue} Button Main`;
+    const baseY = sizeIndex * 900;
+    const instanceY = 200 + baseY;
+    button.y = baseY;
+    nodes.push(button);
+
+    layouts.forEach(layout => {
+      if (layout === 'withoutIcon') {
+        states.forEach((state, i) => {
+          const name = `${msg.styleValue} Button / ${size} / false / ${state}`;
+          const instanceComponent = createInstanceComponent({ main: button, x: i * 300, y: instanceY, name, layout })
+          figma.currentPage.appendChild(instanceComponent);
+          nodes.push(instanceComponent);
+        })
+      }
+
+      if (layout === 'withIcon') {
+        states.forEach((state, i) => {
+          const name = `${msg.styleValue} Button / ${size} / true / ${state}`;
+          const instanceComponent = createInstanceComponent({ main: button, x: i * 300, y: instanceY + 200, name, layout });
+          figma.currentPage.appendChild(instanceComponent);
+          nodes.push(instanceComponent);
+        })
+      }
+    })
+
+    return nodes;
+  }
+
+  figma.ui.onmessage = msg => {
+    let nodes: SceneNode[] = [];
+    if (msg.type === 'button-maker') {
+
+      sizes.forEach((size, i) => {
+        const buttons = adjustButtonBySize(size, msg, i);
+        nodes = nodes.concat(buttons);
       })
 
       figma.currentPage.selection = nodes;
