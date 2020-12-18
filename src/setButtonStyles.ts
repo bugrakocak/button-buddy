c5ba3f6939b1bd768cd22c07e555f49348a56294
Buğra Koçak <mbkocak2@gmail.com>
2020-12-18 22:04:24 +0300

    Update button styles

--- src/setButtonStyles.ts
+++ src/setButtonStyles.ts
@@ -1,40 +1,35 @@
-import { rgb } from "color";
 import { hexToRgbUnitObject } from "./utils";
 
 const setButtonStyles = (button, buttonStyle, primaryColor) => {
-  const rgbUnit = hexToRgbUnitObject(primaryColor);
+  const primaryColorAsRgbUnit = hexToRgbUnitObject(primaryColor);
 
   if (buttonStyle === 'basicSolid') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
   }
 
   if (buttonStyle === 'basicOutline') {
     button.fills = [];
     button.strokeWeight = 2;
-    button.strokes = [{type: 'SOLID', color: rgbUnit}];
+    button.strokes = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
     const textNode = (button.findAll((node) => node.type === 'TEXT')[0] as any);
-    textNode.fills = [{type: 'SOLID', color: rgbUnit }];
     const vector = button.findAll((node) => node.type === 'VECTOR')[0] as any;
-    vector.fills = [{ type: 'SOLID', color: rgbUnit}];
+    textNode.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit }];
+    vector.fills = [{ type: 'SOLID', color: primaryColorAsRgbUnit}];
   }
 
   if (buttonStyle === 'modern') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
-    button.effects = [{type: 'DROP_SHADOW', color: {...rgbUnit, a: .4}, offset: {x: 0, y: 4}, radius: 16, visible: true, blendMode: 'NORMAL'}]
-    const textNode = (button.findAll((node) => node.type === 'TEXT')[0] as any);
-    const vector = (button.findAll((node) => node.type === 'VECTOR')[0] as any);
-    textNode.fills = [{type: 'SOLID', color: rgbUnit }];
-    vector.fills = [{ type: 'SOLID', color: rgbUnit}];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
+    button.effects = [{type: 'DROP_SHADOW', color: {...primaryColorAsRgbUnit, a: .4}, offset: {x: 0, y: 4}, radius: 16, visible: true, blendMode: 'NORMAL'}]
   }
 
 //TODO// Shadow should be 0.3 darker than the original color.
   if (buttonStyle === 'flatShadow') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
-    button.effects = [{type: 'DROP_SHADOW', color: {...rgbUnit, b: rgbUnit.b * .8 , a: 1}, offset: {x: 0, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
+    button.effects = [{type: 'DROP_SHADOW', color: {...primaryColorAsRgbUnit, b: primaryColorAsRgbUnit.b * .8 , a: 1}, offset: {x: 0, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }];
   }
 
   if (buttonStyle === 'glossy') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
     button.effects = [
       {type: 'INNER_SHADOW', color: {r: 0/255, g: 0/255, b: 0/255, a: 0.2}, offset: {x: 0, y: -8}, radius: 10, visible: true, blendMode: 'NORMAL' },
       {type: 'INNER_SHADOW', color: {r: 255/255, g: 255/255, b: 255/255, a: 0.5}, offset: {x: 0, y: 4}, radius: 4, visible: true, blendMode: 'NORMAL' }
@@ -44,27 +39,27 @@ const setButtonStyles = (button, buttonStyle, primaryColor) => {
   if (buttonStyle === 'comicLight') {
     button.fills = [{type: 'SOLID', color: {r: 255/255, g: 255/255, b: 255/255}}];
     button.strokeWeight = 2;
-    button.strokes = [{type: 'SOLID', color: rgbUnit}];
-    button.effects = [{type: 'DROP_SHADOW', color: {...rgbUnit, a: 1}, offset: {x: -4, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }]
+    button.strokes = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
+    button.effects = [{type: 'DROP_SHADOW', color: {...primaryColorAsRgbUnit, a: 1}, offset: {x: -4, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }]
 
     const textNode = (button.findAll((node) => node.type === 'TEXT')[0] as any);
     const vector = (button.findAll((node) => node.type === 'VECTOR')[0] as any);
-    textNode.fills = [{type: 'SOLID', color: rgbUnit }];
-    vector.fills = [{ type: 'SOLID', color: rgbUnit}];
+    textNode.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit }];
+    vector.fills = [{ type: 'SOLID', color: primaryColorAsRgbUnit}];
   }
 
   if (buttonStyle === 'comicBold') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
     button.strokeWeight = 2;
     button.strokes = [{type: 'SOLID', color: {r: 255/255, g: 255/255, b: 255/255}}];
-    button.effects = [{type: 'DROP_SHADOW', color: {...rgbUnit, a: 1}, offset: {x: -4, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }]
+    button.effects = [{type: 'DROP_SHADOW', color: {...primaryColorAsRgbUnit, a: 1}, offset: {x: -4, y: 4}, radius: 0, visible: true, blendMode: 'NORMAL' }]
   }
 
   if (buttonStyle === 'fancy') {
-    button.fills = [{type: 'SOLID', color: rgbUnit}];
+    button.fills = [{type: 'SOLID', color: primaryColorAsRgbUnit}];
     button.strokeWeight = 2;
     button.strokes = [{type: 'SOLID', color: {r: 255/255, g: 255/255, b: 255/255}}];
-    button.effects = [{type: 'DROP_SHADOW', color: {...rgbUnit, a: 0.4}, offset: {x: 0, y: 4}, radius: 16, visible: true, blendMode: 'NORMAL' }]
+    button.effects = [{type: 'DROP_SHADOW', color: {...primaryColorAsRgbUnit, a: 0.4}, offset: {x: 0, y: 4}, radius: 16, visible: true, blendMode: 'NORMAL' }]
   }
 
   return button;
