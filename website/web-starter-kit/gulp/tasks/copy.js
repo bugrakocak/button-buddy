9f9ff64182beae02f27e08a7cc433875347202d4
Buğra Koçak <mbkocak2@gmail.com>
2020-12-20 23:53:44 +0300

    Add wsk for website

---
+++ website/web-starter-kit/gulp/tasks/copy.js
@@ -0,0 +1,22 @@
+import gulp, { parallel } from 'gulp';
+
+import paths from '../../config/paths';
+import { envPath } from '../../config/env';
+
+export function copyImages() {
+  return gulp
+    .src(`${paths.img}/**/*`)
+    .pipe(gulp.dest(`${paths.styleguide.dist}/${paths.styleguide.assets.images}`));
+}
+
+export function copyFonts() {
+  return gulp.src(`${paths.fonts}/**/*`).pipe(gulp.dest(`${envPath}/${paths.assets.fonts}`));
+}
+
+export function copyPublic() {
+  return gulp
+    .src([`${paths.public}/**/*`, `!${paths.public}/.gitkeep`], { dot: true })
+    .pipe(gulp.dest(`${envPath}/`));
+}
+
+export default parallel(copyImages, copyFonts, copyPublic);
