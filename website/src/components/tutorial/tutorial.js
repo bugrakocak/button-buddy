ca68ca3b5333d3c847a37ffc7138020a314de659
Buğra Koçak <mbkocak2@gmail.com>
2021-01-16 18:57:06 +0300

    Update tutorial section

--- website/src/components/tutorial/tutorial.js
+++ website/src/components/tutorial/tutorial.js
@@ -1,35 +1,68 @@
+/* eslint-disable no-param-reassign */
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/src/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const video = document.getElementsByClassName('js-tutorial-video')[0];
-const videoSource = video.getElementsByTagName('source')[0];
+const tutorial = document.getElementById('tutorial');
+const videos = document.getElementsByClassName('js-tutorial-video');
 
 const tl = gsap.timeline();
 
 const timingFunction = 'power3.out';
 
-function changeVideo(videoName) {
-  videoSource.src = `img/${videoName}.webm`;
-  video.load();
-}
+const sectionMaterialMap = {
+  s1: {
+    videoIndex: 0,
+    figureClassName: 'tutorial--figures-1',
+  },
+  s2: {
+    videoIndex: 1,
+    figureClassName: 'tutorial--figures-2',
+  },
+  s3: {
+    videoIndex: 2,
+    figureClassName: 'tutorial--figures-3',
+  },
+};
 
-function videoController() {
+const changeVideo = (label) => {
+  Array.from(videos).forEach((video, index) => {
+    if (index === sectionMaterialMap[label].videoIndex) {
+      video.hidden = false;
+      return;
+    }
+    video.hidden = true;
+  });
+};
+
+const changeFigures = (label) => {
+  const modifierClassIndex = Array.from(tutorial.classList).findIndex((item) =>
+    item.includes('--figures'),
+  );
+
+  const modifierClass = tutorial.classList.item(modifierClassIndex);
+  tutorial.classList.replace(modifierClass, sectionMaterialMap[label].figureClassName);
+};
+
+const updateMaterials = () => {
   const label = tl.currentLabel();
 
   if (label === 's1') {
-    changeVideo('v1');
+    changeVideo(label);
+    changeFigures(label);
   }
 
   if (label === 's2') {
-    changeVideo('v2');
+    changeVideo(label);
+    changeFigures(label);
   }
 
   if (label === 's3') {
-    changeVideo('v3');
+    changeVideo(label);
+    changeFigures(label);
   }
-}
+};
 
 tl.addLabel('s1')
   .from('.js-tutorial-s1', {
@@ -39,7 +72,7 @@ tl.addLabel('s1')
     ease: timingFunction,
   })
   .to('.js-tutorial-s1', { opacity: 0, y: -100, duration: 0.2, ease: timingFunction }, '+=0.2')
-  .call(videoController)
+  .call(updateMaterials)
   .addLabel('s2')
   .from('.js-tutorial-s2', {
     opacity: 0,
@@ -57,7 +90,7 @@ tl.addLabel('s1')
     },
     '+=0.2',
   )
-  .call(videoController)
+  .call(updateMaterials)
   .addLabel('s3')
   .from('.js-tutorial-s3', {
     opacity: 0,
@@ -66,7 +99,7 @@ tl.addLabel('s1')
     ease: timingFunction,
   })
   .to('.js-tutorial-s3', { opacity: 0, y: -100, duration: 0.2, ease: timingFunction }, '+=0.2')
-  .call(videoController);
+  .call(updateMaterials);
 
 ScrollTrigger.create({
   animation: tl,
@@ -83,3 +116,4 @@ ScrollTrigger.create({
   end: '+=2000',
   scrub: 0.5,
 });
+/* eslint-enable no-param-reassign */
