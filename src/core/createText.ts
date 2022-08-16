1b54fc265111b496b0360513a7984d5f3dd27a9c
bugra <mbkocak2@gmail.com>
2022-08-17 00:40:07 +0300

    Fix error

--- src/core/createText.ts
+++ src/core/createText.ts
@@ -2,13 +2,13 @@ import { hexToRgbUnitObject } from '../utils';
 
 const createText = ({ fontSize, lineHeight, color }) => {
   const text = figma.createText();
-  text.fontSize = fontSize;
-  text.lineHeight = lineHeight;
-  text.characters = 'BUTTON';
   text.fontName = {
     family: 'Inter',
     style: 'Bold',
   };
+  text.fontSize = fontSize;
+  text.lineHeight = lineHeight;
+  text.characters = 'BUTTON';
   text.fills = [{ type: 'SOLID', color: hexToRgbUnitObject(color) }];
 
   return text;
